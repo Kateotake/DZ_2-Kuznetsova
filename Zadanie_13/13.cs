a8108b95d9dca8a7ef4dddf5ca593e1fb6b8d6bb
KuznetsovaE <kettt56@mail.ru>
2022-08-27 03:01:18 +0300

    DZ2

---
+++ Zadanie_13/13.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write ("Введите целое число ");
+string chislo = Console.ReadLine();
+int dlina_chisla = chislo.Length; 
+int N = Convert.ToInt32(chislo); 
+int i = 0;
+
+if (dlina_chisla >= 3)
+{
+    while (i < (dlina_chisla - 3)) //пока i не дойдет третьей цифры , делим N на 10
+        {
+            N = N/10;
+            i++;
+        }
+
+Console.WriteLine(N % 10);
+}
+else  Console.WriteLine ("третьей цифры нет");
+
+
---
+++ Zadanie_15/15.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру от 1 до 7 ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a == 1) Console.WriteLine("Нет");
+else if(a == 2) Console.WriteLine("Нет");
+else if(a == 3) Console.WriteLine("Нет");
+else if(a == 4) Console.WriteLine("Нет");
+else if(a == 5) Console.WriteLine("Нет");
+else if(a == 6) Console.WriteLine("Да");
+else if(a ==7) Console.WriteLine("Да");
+else Console.WriteLine("Такого дня недели я не знаю");
